7515652c91d2bf6ed142160e59aac3c4a473c776
Pawel Sokolowski <Pawel.Sokolowski1@ey.com>
2021-06-17 16:48:45 -0400

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace samplewebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string ID { get; set; }
+        public string InstanceName { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ID = Dns.GetHostName();
+            InstanceName = _configuration["InstanceName"];
+        }
+    }
+}
